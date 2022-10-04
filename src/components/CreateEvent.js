7e251a126119d63885a10f92160fcefdc8facd55
Ryan Howard <ryanghoward@protonmail.com>
2022-10-04 15:45:38 -0700

    react-icons

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 const CreateEvent = () => {
   const [formValues, setFormValues] = useState({
-    name: "",
-    organizer: "",
-    date: "",
-    location: "",
-    guests: "",
-    food: "",
+    eventName: "",
+    hostName: "",
+    startTime: "",
+    endTime: "",
+    eventLocation: "",
+    eventPhoto: "",
   });
   const [error, setError] = useState("");
   const { push } = useNavigate();
@@ -16,12 +16,12 @@ const CreateEvent = () => {
   const submit = (e) => {
     e.preventDefault();
     const newEvent = {
-      name: formValues.name,
-      organizer: formValues.organizer,
-      date: formValues.date,
-      location: formValues.location,
-      guests: formValues.guests,
-      food: formValues.food,
+      eventName: formValues.eventName,
+      hostName: formValues.hostName,
+      startTime: formValues.startTime,
+      endTime: formValues.endTime,
+      eventLocation: formValues.location,
+      eventPhoto: formValues.eventPhoto,
     };
   };
 
@@ -39,6 +39,55 @@ const CreateEvent = () => {
       <div>
         <form onSubmit={submit}>
           <h2>Start Planning!</h2>
+          <p>Event Name:</p>
+          <input
+            value={formValues.eventName}
+            name='eventName'
+            type='text'
+            onChange={change}
+            placeholder='Enter your name'
+          />
+          <p>Host Name:</p>
+          <input
+            value={formValues.hostName}
+            name='hostName'
+            type='text'
+            onChange={change}
+            placeholder='Host of Event'
+          />
+          <p>Start Time:</p>
+          <input
+            value={formValues.startTime}
+            name='startTime'
+            type='text'
+            onChange={change}
+            placeholder='Event Start Time'
+          />
+          <p>End Time:</p>
+          <input
+            value={formValues.endTime}
+            name='endTime'
+            type='text'
+            onChange={change}
+            placeholder='Event End Time'
+          />
+          <p>Location:</p>
+          <input
+            value={formValues.eventLocation}
+            name='eventLocation'
+            type='text'
+            onChange={change}
+            placeholder='Event Location'
+          />
+          {/* <p>Photo:</p>
+          <input
+            value={formValues.eventPhoto}
+            name='eventPhoto'
+            type='photo'
+            onChange={change}
+            placeholder='Upload an event photo'
+          /> */}
+          <input type='submit' value='Schedule Event' />
         </form>
       </div>
     </>
