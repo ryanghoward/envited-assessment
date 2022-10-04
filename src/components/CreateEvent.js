bc2a1354f0aaa39c81b4819bc3cbbea838ae5d2d
Ryan Howard <ryanghoward@protonmail.com>
2022-10-04 16:18:52 -0700

    save

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,7 +1,46 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../App.css";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { Link } from "react-router-dom";
+
+import { GiPartyPopper } from "react-icons/gi";
+import { BsFillPersonFill, BsCameraFill } from "react-icons/bs";
+import { GoCalendar } from "react-icons/go";
+import { FaFlagCheckered, FaMapPin } from "react-icons/fa";
+
+const UploadImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <div className='uploadImg'>
+      {selectedImage && (
+        <div>
+          <img
+            alt='not found'
+            width={"250px"}
+            src={URL.createObjectURL(selectedImage)}
+          />
+          <br />
+          <button onClick={() => setSelectedImage(null)}>Remove</button>
+        </div>
+      )}
+      <input
+        type='file'
+        name='myImage'
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
+        }}
+      />
+    </div>
+  );
+};
 
 const CreateEvent = () => {
+  const [error, setError] = useState("");
+  const { push } = useNavigate();
+
   const [formValues, setFormValues] = useState({
     eventName: "",
     hostName: "",
@@ -10,8 +49,17 @@ const CreateEvent = () => {
     eventLocation: "",
     eventPhoto: "",
   });
-  const [error, setError] = useState("");
-  const { push } = useNavigate();
+
+  const sendItems = (newEvent) => {
+    axiosWithAuth()
+      .post(newEvent)
+      .then((res) => {
+        push("/events");
+      })
+      .catch((err) => {
+        setError("Sorry, an error occurred");
+      });
+  };
 
   const submit = (e) => {
     e.preventDefault();
@@ -23,6 +71,7 @@ const CreateEvent = () => {
       eventLocation: formValues.location,
       eventPhoto: formValues.eventPhoto,
     };
+    sendItems(newEvent);
   };
 
   const handleClick = () => {
@@ -35,11 +84,13 @@ const CreateEvent = () => {
   };
 
   return (
-    <>
-      <div>
-        <form onSubmit={submit}>
-          <h2>Start Planning!</h2>
-          <p>Event Name:</p>
+    <div className='createEventForm'>
+      <form onSubmit={submit}>
+        <h1>Start Planning!</h1>
+        <GiPartyPopper />
+        Event Name
+        <br />
+        <label>
           <input
             value={formValues.eventName}
             name='eventName'
@@ -47,50 +98,53 @@ const CreateEvent = () => {
             onChange={change}
             placeholder='Enter your name'
           />
-          <p>Host Name:</p>
+        </label>
+        <BsFillPersonFill />
+        Host Name
+        <label>
           <input
             value={formValues.hostName}
             name='hostName'
             type='text'
             onChange={change}
-            placeholder='Host of Event'
+            placeholder='Host Of Event'
           />
-          <p>Start Time:</p>
-          <input
-            value={formValues.startTime}
-            name='startTime'
-            type='text'
-            onChange={change}
-            placeholder='Event Start Time'
-          />
-          <p>End Time:</p>
-          <input
-            value={formValues.endTime}
-            name='endTime'
-            type='text'
-            onChange={change}
-            placeholder='Event End Time'
-          />
-          <p>Location:</p>
-          <input
-            value={formValues.eventLocation}
-            name='eventLocation'
-            type='text'
-            onChange={change}
-            placeholder='Event Location'
-          />
-          {/* <p>Photo:</p>
-          <input
-            value={formValues.eventPhoto}
-            name='eventPhoto'
-            type='photo'
-            onChange={change}
-            placeholder='Upload an event photo'
-          /> */}
+        </label>
+        <GoCalendar />
+        Start Time
+        <input
+          value={formValues.startTime}
+          name='startTime'
+          type='text'
+          onChange={change}
+          placeholder='Event Start Time'
+        />
+        <FaFlagCheckered />
+        End Time
+        <input
+          value={formValues.endTime}
+          name='endTime'
+          type='text'
+          onChange={change}
+          placeholder='Event End Time'
+        />
+        <FaMapPin />
+        Location
+        <input
+          value={formValues.eventLocation}
+          name='eventLocation'
+          type='text'
+          onChange={change}
+          placeholder='Event Location'
+        />
+        <BsCameraFill />
+        Photo
+        <UploadImage />
+        <Link to='/event'>
           <input type='submit' value='Schedule Event' />
-        </form>
-      </div>
-    </>
+        </Link>
+      </form>
+    </div>
   );
 };
 
