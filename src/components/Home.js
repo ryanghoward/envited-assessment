f8c6f0aa317be9771c15a5c1992c0e9fe267d22c
Ryan Howard <ryanghoward@protonmail.com>
2022-10-04 14:53:37 -0700

    Hompage is decent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,25 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "../App.css";
-import img from "../assets/LPImg.svg";
 
 const Home = () => {
   return (
     <div className='homepage'>
-      <div className='header'>
-        <div className='text-container'>
-          <h2 className='title'>
-            Imagine if <br />
-          </h2>
-          <h2 className='gradient'>Snapchat</h2>
-          <h2 className='title'>had events.</h2>
+      <div className='wrapper'>
+        <div className='image-wrapper'></div>
+        <div className='text-wrapper'>
+          <h1 className='title-1'>Imagine if</h1>
+          <h2 className='text gradient'>Snapchat</h2>
+          <h1 className='title-2'>had events.</h1>
           <p className='tagline'>
-            Easily host and share events with your friends across any social
-            media
+            Easily host and share events with your friends <br />
+            across any social media.
           </p>
+          {/* <Link to='/create'> */}
+          <button className='button'>🎉 Create My Event</button>
+          {/* </Link> */}
         </div>
       </div>
-      {/* <img src={img} alt='pic' /> */}
-      {/* <Link to='/create'> */}
-      <button className='button'>🎉 Create My Event</button>
-      {/* </Link> */}
     </div>
   );
 };
